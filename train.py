9911e6baf4f9fa80ca8e65ea6b8cf4a88d0bd37f
DreamAndDead <favorofife@yeah.net>
2020-03-02 10:35:51 +0800

    init

---
+++ train.py
@@ -0,0 +1,141 @@
+import os
+import h5py
+import numpy as np
+import keras
+import yaml
+from keras.callbacks import ModelCheckpoint
+
+
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+def create_model():
+    model = keras.models.Sequential([
+        keras.layers.Dense(512, activation="relu", input_shape=(784,)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss=keras.losses.sparse_categorical_crossentropy,
+                  metrics=['accuracy'])
+
+    return model
+
+
+model = create_model()
+model.summary()
+
+
+output_dir = './output/'
+
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
+
+class MetaCheckpoint(ModelCheckpoint):
+    def __init__(self, filepath, monitor='val_loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1, training_args=None, meta=None):
+
+        super(MetaCheckpoint, self).__init__(filepath,
+                                             monitor=monitor,
+                                             verbose=verbose,
+                                             save_best_only=save_best_only,
+                                             save_weights_only=save_weights_only,
+                                             mode=mode,
+                                             period=period)
+
+        self.filepath = filepath
+        self.new_file_override = True
+        self.meta = meta or {'epochs': [], self.monitor: []}
+
+        if training_args:
+            self.meta['training_args'] = training_args
+
+    def on_train_begin(self, logs={}):
+        if self.save_best_only:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.best = max(self.meta[self.monitor], default=-np.Inf)
+            else:
+                self.best = min(self.meta[self.monitor], default=np.Inf)
+
+        super(MetaCheckpoint, self).on_train_begin(logs)
+
+    def on_epoch_end(self, epoch, logs={}):
+        if self.save_best_only:
+            current = logs.get(self.monitor)
+            if self.monitor_op(current, self.best):
+                self.new_file_override = True
+            else:
+                self.new_file_override = False
+
+        super(MetaCheckpoint, self).on_epoch_end(epoch, logs)
+
+        # Get statistics
+        self.meta['epochs'].append(epoch)
+        for k, v in logs.items():
+            # Get default gets the value or sets (and gets) the default value
+            self.meta.setdefault(k, []).append(v)
+
+        # Save to file
+        filepath = self.filepath.format(epoch=epoch, **logs)
+
+        if self.new_file_override and self.epochs_since_last_save == 0:
+            # 只有在‘只保存’最优版本且生成新的.h5文件的情况下 才会继续添加meta
+            with h5py.File(filepath, 'r+') as f:
+                meta_group = f.create_group('meta')
+                meta_group.attrs['training_args'] = yaml.dump(
+                    self.meta.get('training_args', '{}'))
+                meta_group.create_dataset('epochs', data=np.array(self.meta['epochs']))
+                for k in logs:
+                    meta_group.create_dataset(k, data=np.array(self.meta[k]))
+
+
+
+def load_meta(model_fname):
+    """
+    Load meta configuration
+    :param model_fname: model file name
+    :return: meta info
+    """
+    meta = {}
+
+    with h5py.File(model_fname, 'r') as f:
+        meta_group = f['meta']
+
+        meta['training_args'] = yaml.load(
+            meta_group.attrs['training_args'])
+        for k in meta_group.keys():
+            meta[k] = list(meta_group[k])
+
+    return meta
+
+
+output_net_path = os.path.join(output_dir, 'pointnet.h5')
+
+def get_last_status(model):
+    last_epoch = -1
+    last_meta = {}
+    if os.path.exists(output_net_path):
+        model.load_weights(output_net_path)
+        last_meta = load_meta(output_net_path)
+        last_epoch = last_meta.get('epochs')[-1]
+    return last_epoch, last_meta
+
+
+last_epoch, last_meta = get_last_status(model)
+
+checkpoint = MetaCheckpoint(output_net_path, monitor='val_acc',
+                            save_weights_only=True, save_best_only=True,
+                            verbose=1, meta=last_meta)
+
+model.fit(train_images, train_labels, epochs=10,
+          validation_data=(test_images, test_labels),
+          callbacks=[checkpoint],
+          initial_epoch=last_epoch + 1)
